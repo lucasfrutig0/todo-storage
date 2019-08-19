95529a5b014048231ad076afbc1eb23a0570ec53
unknown <lucasfrutig0@gmail.com>
2019-08-19 19:53:24 -0300

    Updates

---
+++ precache-manifest.908ac3a87fa6c7137217ec25ed84d1af.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3f42aeebaff6fbe81d969739625bf3",
+    "url": "/todo-storage/index.html"
+  },
+  {
+    "revision": "2f3cc32d6498e372ba86",
+    "url": "/todo-storage/static/js/2.ab8af9ab.chunk.js"
+  },
+  {
+    "revision": "114e716ddbd7e49daf6a",
+    "url": "/todo-storage/static/js/main.46e6b302.chunk.js"
+  },
+  {
+    "revision": "a722a015542d9240ef20",
+    "url": "/todo-storage/static/js/runtime~main.3dc04269.js"
+  }
+]);
